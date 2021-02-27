8bed427d7a55ed4f31cd4f21ff01c8a7133af967
orange-r <oranger0702@gmail.com>
2021-02-28 05:31:14 +0900

    twitter通信できるとこまで(use needle)
    - npm install needle

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,31 @@
+import needle = require("needle");
+
+let token = process.env.BEARER_TOKEN;
+let endpointUrl = "https://api.twitter.com/2/tweets/search/recent";
 
 exports.handler = (event: any, context: any, callback: Function) => {
-  console.warn('[From Typescript]');
-  console.warn('Event not processed.');
+  console.info('[From Typescript]');
+  console.info('Event not processed.');
+
+  console.info(token);
+
+  let params = {
+    'query': 'from:twitterdev -is:retweet',
+    'tweet.fields': 'author_id'
+  }
+
+  let headers = {
+      "User-Agent": "v2RecentSearchJS",
+      "authorization": `Bearer ${token}`
+    }
+
+  needle('get', endpointUrl, params, { headers: headers })
+  .then( (response: needle.NeedleResponse) => {
+    console.info(response.body);
+  })
+  .catch( (err: any) => {
+    throw new Error('Unsuccessful request');
+  })
 
   callback(null, 'Hello from Lambda with Typescript');
 }
