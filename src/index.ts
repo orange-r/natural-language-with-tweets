0d7feb0d7d935b5b6275ccb5e8343664157222e2
orange-r <oranger0702@gmail.com>
2021-03-07 02:14:39 +0900

    twitterからツイート取得まで確認

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import needle = require("needle");
+import needle = require('needle');
 import { format } from 'date-fns'
 import { utcToZonedTime } from 'date-fns-tz'
 import { ja } from 'date-fns/locale'
@@ -10,8 +10,15 @@ let aws = require('aws-sdk')
 const s3 = new aws.S3()
 
 // Twitter
+const Twitter = require('twitter')
+const client = new Twitter({
+  consumer_key        : process.env.API_KEY ,           // consumer keyを記入
+  consumer_secret     : process.env.API_KEY_SECRET,     // consumer secretを記入
+  access_token_key    : process.env.ACCESS_TOKEN,       // access tokenを記入
+  access_token_secret : process.env.ACCESS_TOKEN_SECRET // access token secretを記入
+});
 let token = process.env.BEARER_TOKEN;
-let endpointUrl = "https://api.twitter.com/2/tweets/search/recent";
+let endpointUrl = 'https://api.twitter.com/2/tweets/search/recent';
 
 exports.handler = async (event: any, context: any, callback: Function) => {
   // 現在時刻(Tokyo)
@@ -29,19 +36,57 @@ exports.handler = async (event: any, context: any, callback: Function) => {
   }
 
   let headers = {
-      "User-Agent": "v2RecentSearchJS",
-      "authorization": `Bearer ${token}`
+      'User-Agent': 'v2RecentSearchJS',
+      'authorization': `Bearer ${token}`
     }
 
-  needle('get', endpointUrl, params, { headers: headers })
-  .then( (response: needle.NeedleResponse) => {
-    console.info(response.body);
-  })
-  .catch( (err: any) => {
-    throw new Error('Unsuccessful request');
-  })
+  // needle('get', endpointUrl, params, { headers: headers })
+  // .then( (response: needle.NeedleResponse) => {
+    // console.info(response.body);
+  // })
+  // .catch( (err: any) => {
+    // throw new Error('Unsuccessful request');
+  // })
 
-  // TODO: S3へ書き出す(yyyy-mm-dd/)
+  // Twitterからデータ取得
+  try {
+    let tweets = await client.get('search/tweets', { q: 'node.js', count: 3, locale: 'ja' } );
+    console.log('--- resulrt of getting Twitter ---');
+    console.log('--- tweets ---');
+    console.table(tweets.statuses.length);
+    console.log('--- tweets[0] ---');
+    // console.dir(tweets.statuses[0], { depth: null });
+    // console.log(tweets.statuses[0].text);
+    console.log('--- for tweet in tweets ---');
+    for (let it in tweets.statuses) {
+      let tweet = tweets.statuses[it];
+      console.dir(tweet, { depth: null });
+      console.log('------');
+      // console.log(tweets.statuses[0].text);
+      // console.info(`${tweet.created_at}: ${tweet.text}`);
+    }
+    // console.log('--- each tweet of tweets ---');
+    // for (let tweet of tweets.statuses) {
+    //   console.dir(tweet, { depth: null });
+    //   console.log('------');
+    //   console.info(`${tweet.created_at}: ${tweet.text}`);
+    // }
+    console.log('--- each tweet of tweets ---');
+    for (let tweet of tweets.statuses) {
+      console.log('+-----------+');
+      console.info(tweet.created_at);
+      console.info(tweet.text);
+      console.log('+-----------+');
+    }
+    // console.log('--- response ---');
+    // console.log(response);
+  } catch (error) {
+     console.warn('ERROR: Twitter.client.get');
+     console.warn(error, error.stack);
+    throw error;
+  }
+
+  // S3へ書き出す(yyyy-mm-dd/)
   let destparams = {
     Bucket: 'natural-language-with-tweets',
     Key: `${format(jstDate, 'yyyy-MM-dd', {locale: ja})}/file.text`,
@@ -52,8 +97,8 @@ exports.handler = async (event: any, context: any, callback: Function) => {
   try {
     let putResult = await s3.putObject(destparams).promise();
      console.log(putResult);
-  } catch(err) {
-     console.warn(err, err.stack);
+  } catch(error) {
+     console.warn(error, error.stack);
   }
 
 
