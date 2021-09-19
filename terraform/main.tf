fda07a8e5f0b188756b5553ee9c01e1c9dea9a00
orange-r <oranger0702@gmail.com>
2021-09-19 21:34:00 +0900

    CloudWatch Eventsの設定追加

--- terraform/main.tf
+++ terraform/main.tf
@@ -32,11 +32,11 @@ resource "aws_iam_role_policy" "iam_for_lambda" {
 }
 
 resource "aws_lambda_function" "default" {
-  function_name = "lambda_container"
+  function_name = "natural-language-with-tweets"
   role          = aws_iam_role.iam_for_lambda.arn
   package_type  = "Image"
   image_uri     = "${data.aws_ecr_repository.lambda_containter.repository_url}:latest"
-  timeout       = 600
+  timeout       = 900
 
   lifecycle {
     ignore_changes = [image_uri]
@@ -44,7 +44,34 @@ resource "aws_lambda_function" "default" {
 
   environment {
     variables = {
-      foo = "bar"
+      Opportunity = "k-edo"
     }
   }
+}
+
+# CloudWatch Eventsの設定
+resource "aws_cloudwatch_event_rule" "default" {
+  name                = "natural-language-with-tweets"
+  description         = "毎日 PM 9:00(JST)に実行"
+  schedule_expression = "cron(0 12 * * ? *)"
+}
+
+resource "aws_cloudwatch_event_target" "query1" {
+  rule      = aws_cloudwatch_event_rule.default.name
+  arn       = aws_lambda_function.default.arn
+  input     = "{\"twitterQuery\":\"三代 #ボクシング exclude:retweets\", \"fileName\":\"三代_#ボクシング.csv\"}"
+}
+resource "aws_cloudwatch_event_target" "query2" {
+  rule      = aws_cloudwatch_event_rule.default.name
+  arn       = aws_lambda_function.default.arn
+  input     = "{\"twitterQuery\":\"三代 #ボクサー exclude:retweets\", \"fileName\":\"三代_#ボクサー.csv\"}"
+}
+
+# CloudWatch EventsからLambdaを呼び出せるように設定
+resource "aws_lambda_permission" "allow_cloudwatch" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.default.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.default.arn
 }
